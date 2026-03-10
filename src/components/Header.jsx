dec935f810c0eaa4e40bddb81bbcb1838e904971
vibeclue <most.possible@mail.ru>
2026-03-10 14:02:28 +0300

    create component Header + css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header">
+      <div className="badge">RETRO</div>
+      <h1 className="title">VIBECLUE</h1>
+      <p className="subtitle">Мои ссылки</p>
+    </header>
+  );
 };
 
 export default Header;
