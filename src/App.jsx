af9d390c4be095454d0dd5dcb2f08d17f217648a
vibeclue <most.possible@mail.ru>
2026-03-10 13:57:35 +0300

    git init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="card">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/links.js
@@ -0,0 +1,9 @@
+export const links = [
+  { label: "Youtube", url: "https://www.youtube.com/@vibeclue" },
+  { label: "Youtube-gacha", url: "https://www.youtube.com/@Vibeclue-gacha" },
+  { label: "Telegram", url: "t.me/vibeclue" },
+  { label: "Boosty", url: "boosty.to/vibeclue" },
+  { label: "Patreon", url: "patreon.com/cw/VIBECLUE" },
+  { label: "Twitch", url: "twitch.tv/vibeclue" },
+  { label: "Cookies", url: "donationalerts.com/r/vibeclue" },
+];
